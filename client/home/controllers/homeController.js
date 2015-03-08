0573f036e80337c5d14e175ae14795a4c2f97345
Deon Hua <deonhbci@gmail.com>
2015-03-07 21:15:02 -0500

    Minor redesign of homepage.

--- client/home/controllers/homeController.js
+++ client/home/controllers/homeController.js
@@ -1,3 +1,6 @@
 angular.module("roomi").controller("HomeController", ['$scope', '$meteor', function($scope, $meteor){
 
+    $scope.messages = [{user:"Jane", message: "Semi's this Friday, anyone wanna go?"}, {user:"John", message: "Sure, let's go!"}, {user:"Bob", message:"Sure, why not?"}];
+    $scope.nextBill = {date:"March 9", cost:"99", person:"Waterloo Hydro"};
+    $scope.nextEvent = {date:"March 12", title:"Suite Cleanup"};
 }]);
