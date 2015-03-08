ce7a0c8d260c402eecada2d7cdf7c115ab3eb2cf
Yuwei Xu <yuwei_xu@outlook.com>
2015-03-08 16:49:04 -0400

    changed some dummy data

--- client/home/controllers/homeController.js
+++ client/home/controllers/homeController.js
@@ -11,9 +11,9 @@ angular.module("roomi").controller("HomeController", ['$scope', '$meteor', '$roo
     });
     $scope.messages = [{user:"Jane", message: "Semi's this Friday, anyone wanna go?"}, {user:"John", message: "Sure, let's go!"}, {user:"Bob", message:"Sure, why not?"}];
     $scope.nextBill = {date:"March 9", cost:"99", person:"Waterloo Hydro"};
-    $scope.nextEvent = {date:"March 12", title:"Jane's Birthday Dinner!"};
-    $scope.roommateStatus = [{name:"Jane", status:"Away"}, {name:"John", status:"Home"}, {name:"Bob", status:"Do Not Disturb"}];
+    $scope.nextEvent = {date:"March 12", title:"RuoTai's Birthday Dinner!"};
+    $scope.roommateStatus = [{name:"Yu Chen Hou", status:"Away"}, {name:"Yuwei Xu", status:"Home"}, {name:"Deon Hua", status:"Do Not Disturb"},{name:"RuoTai Sun", status:"Do Not Disturb"}];
     $scope.todos = [{name:"Restock on Redbull", date:"Mar 3, 2015"}, {name:"Get More Milk", date:"Mar 7, 2015"}, {name:"Go to Res Meeting", date:"Mar 6, 2015"}];
     $scope.currentUser = $rootScope.currentUser;
-    $scope.nextBirthday = {name:"Jane", date: "April 18, 2015"};
+    $scope.nextBirthday = {name:"RuoTai Sun", date: "April 18, 2015"};
 }]);
