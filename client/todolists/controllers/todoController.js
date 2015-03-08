b5c3a6f083ba085395f12987fc2f2b45916d98c7
Yu Chen Hou <houyuchen66@gmail.com>
2015-03-08 14:48:56 -0400

    signout

--- client/todolists/controllers/todoController.js
+++ client/todolists/controllers/todoController.js
@@ -3,6 +3,7 @@ angular.module("roomi").controller("TodoController", ['$scope', '$meteor', '$roo
 
         accountService.then(function(data){
             console.log(data);
+
           $scope.todos = $meteor.collection(function() {
             return Todolist.find({_id:data.group.todolist});
           });
@@ -17,7 +18,17 @@ angular.module("roomi").controller("TodoController", ['$scope', '$meteor', '$roo
         limit: 10
       });
     });
-
+    $scope.toggleChecked = function(num){
+        console.log($scope.todos[0]._id);
+        $meteor.call('setChecked', $scope.todos[0]._id,num).then(
+            function(data){
+              console.log('success responding', data);
+            },
+            function(err){
+              console.log('failed', err);
+            }
+      );
+    }
     //method called when the user says something
     $scope.add = function(todoInfo){
       $meteor.call('add', todoInfo).then(
