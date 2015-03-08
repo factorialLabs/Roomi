66d7e9188d3c05559844d43a49c4172dc31ffd78
Deon Hua <deonhbci@gmail.com>
2015-03-08 10:16:18 -0400

    A few fixes to To-do

--- client/todolists/controllers/todoController.js
+++ client/todolists/controllers/todoController.js
@@ -1,26 +1,21 @@
 angular.module("roomi").controller("TodoController", ['$scope', '$meteor', '$rootScope','accountService',
     function($scope, $meteor, $rootScope, accountService){
-        console.log(accountService);
 
         accountService.then(function(data){
+            console.log(data);
           $scope.todos = $meteor.collection(function() {
             return Todolist.find({_id:data.group.todolist});
           });
 
         },function(err){
-
+            console.log(err);
         });
 
     //subscribe to conversations
     $meteor.autorun($scope, function() {
       $meteor.subscribe('Todolist', {
         limit: 10
-        //skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
-       // sort: $scope.getReactively('sort')
       });
-//    , $scope.getReactively('search')).then(function() {
-//        $scope.partiesCount = $meteor.object(Counts ,'numberOfParties', false);
-//      });
     });
 
     //method called when the user says something
