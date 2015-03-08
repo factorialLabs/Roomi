3780283ffe6c6cec73726bc80b8e697aee0e9f87
Deon Hua <deonhbci@gmail.com>
2015-03-08 11:34:02 -0400

    Minor fix for group resolution.

--- client/services/accountService.js
+++ client/services/accountService.js
@@ -3,7 +3,7 @@ angular.module("roomi").factory('accountService', ['$rootScope', '$meteor', '$q'
     return $q(function(resolve, reject) {
         $rootScope.currentUserPromise.then(function(user){
             console.log(user);
-            var group = Group.findOne(user.profile.group);
+            var group = Group.findOne({_id:user.profile.group});
             var members = Meteor.users.find({'profile.group':user.profile.group}).fetch();
             var findMembers = $meteor.call("findRoommates", user.profile.group).then(
                 function(roommates){
